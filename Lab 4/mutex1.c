86c5b6c9184805f42052ad7f6e4a2cec1bb6c39f
Bryan <bryanlu.my@gmail.com>
2023-10-23 21:33:42 +0800

    Lab 4 initial commit

---
+++ Lab 4/mutex1.c
@@ -0,0 +1,50 @@
+/**
+ * @file pthread2.c
+ * This program creates two threads that will each print out the global variable after incrementing it using mutex.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+// Mutexes
+pthread_mutex_t mutexA;
+
+// Global variable
+int g_var1 = 0;
+
+void *inc_gv()
+{
+	pthread_mutex_lock(&mutexA);
+	int i, j;
+	for (i = 0; i < 10; i++)
+	{
+		g_var1++; // increment the global variable
+		for (j = 0; j < 5000000; j++)
+			; // delay loop
+		printf("%d ", g_var1);
+		fflush(stdout);
+	}
+	pthread_mutex_unlock(&mutexA);
+}
+
+int main()
+{
+	pthread_t thread1, thread2;
+	pthread_mutex_init(&mutexA, NULL);
+
+	int T1ret, T2ret;
+
+	// Create two threads
+	T1ret = pthread_create(&thread1, NULL, inc_gv, NULL);
+	T2ret = pthread_create(&thread2, NULL, inc_gv, NULL);
+
+	// Main waits for both thread to finnish
+	pthread_join(thread1, NULL);
+	printf("Thread 1 done! \n");
+	pthread_join(thread2, NULL);
+	printf("Thread 2 done! \n");
+
+	printf("Thread 1 and 2 returns: %d, %d \n", T1ret, T2ret);
+	return 0;
+}
---
+++ Lab 4/pthread1.c
@@ -0,0 +1,36 @@
+/**
+ * @file pthread1.c
+ * This program creates two threads that will each print out a message onto the screen.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *print_message(void *ptr)
+{
+	char *text;
+	text = (char *)ptr;
+	printf("%s \n", text);
+	return NULL;
+}
+
+int main()
+{
+	pthread_t thread1, thread2;
+	char *str1 = "I am thread 1!";
+	char *str2 = "I am thread 2!";
+
+	int T1ret, T2ret;
+
+	// Create two threads
+	T1ret = pthread_create(&thread1, NULL, print_message, (void *)str1);
+	T2ret = pthread_create(&thread2, NULL, print_message, (void *)str2);
+
+	// Main waits for both thread to finish
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+
+	printf("Thread 1 and 2 returns: %d, %d \n", T1ret, T2ret);
+	return 0;
+}
